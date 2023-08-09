5ad105dc4ec40682d60214b3913b5ec94e3ae2cc
Paola_G <perfectblue@live.com>
2023-08-09 15:02:40 -0700

    added images to projImages folder(created this folder), added images to Project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,29 +1,37 @@
 import React from 'react';
+import randPass from '../assets/projectImages/random-pass.png';
+import timedQuiz from '../assets/projectImages/timed-quiz.png';
+import workdaySched from '../assets/projectImages/work-day-sched.png';
+import noteTaker from '../assets/projectImages/note-taker.png'
 
 function Project() {
     return(
         <div>
             <div>
                 <h3>Title of First Project</h3>
-                <p>will add image of project</p>
+                <img src={randPass} alt="Random-password-generator"
+                />
                 <p>Link to the deployed application</p>
                 <p>second link to the corresponding Github repository</p>
             </div>
             <div>
                 <h3>Title of Second Project</h3>
-                <p>will add image of project</p>
+                <img src={timedQuiz} alt="Timed-quiz"
+                />
                 <p>Link to the deployed application</p>
                 <p>second link to the corresponding Github repository</p>
             </div>
             <div>
                 <h3>Title of Third Project</h3>
-                <p>will add image of project</p>
+                <img src={workdaySched} alt="Workday-scheduler"
+                />
                 <p>Link to the deployed application</p>
                 <p>second link to the corresponding Github repository</p>
             </div>
             <div>
                 <h3>Title of Fourth Project</h3>
-                <p>will add image of project</p>
+                <img src={noteTaker} alt="Note-taker"
+                />
                 <p>Link to the deployed application</p>
                 <p>second link to the corresponding Github repository</p>
             </div>
