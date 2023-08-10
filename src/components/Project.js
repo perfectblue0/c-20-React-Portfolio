18631adbbe19c4d6ecebe96813b9e67c4484574b
Paola_G <perfectblue@live.com>
2023-08-10 13:16:54 -0700

    changed the project file (added props, removed imported images) and  changed portfolio file so project component is used several times

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,42 +1,23 @@
 import React from 'react';
-import randPass from '../assets/projectImages/random-pass.png';
-import timedQuiz from '../assets/projectImages/timed-quiz.png';
-import workdaySched from '../assets/projectImages/work-day-sched.png';
-import noteTaker from '../assets/projectImages/note-taker.png'
 
-function Project() {
-    return(
-        <div>
-            <div>
-                <h3>Title of First Project</h3>
-                <img src={randPass} alt="Random-password-generator"
-                />
-                <p>Link to the deployed application</p>
-                <p>second link to the corresponding Github repository</p>
-            </div>
-            <div>
-                <h3>Title of Second Project</h3>
-                <img src={timedQuiz} alt="Timed-quiz"
-                />
-                <p>Link to the deployed application</p>
-                <p>second link to the corresponding Github repository</p>
-            </div>
-            <div>
-                <h3>Title of Third Project</h3>
-                <img src={workdaySched} alt="Workday-scheduler"
-                />
-                <p>Link to the deployed application</p>
-                <p>second link to the corresponding Github repository</p>
-            </div>
-            <div>
-                <h3>Title of Fourth Project</h3>
-                <img src={noteTaker} alt="Note-taker"
-                />
-                <p>Link to the deployed application</p>
-                <p>second link to the corresponding Github repository</p>
-            </div>
+const Project = ({ pTitle, image, depLink, gitHLink }) => {
+  
+    const imagePath = require(`../assets/projectImages/${image}`);
+
+    return (
+        <div className="project">
+            <img className="project-image" src={imagePath} alt={pTitle} />
+            <h3 className="project-title">{pTitle}</h3>
+            <div className="project-links">
+            <a className="project-link" href={depLink} target="_blank" rel="noopener noreferrer">
+            Deployed Link
+            </a>
+            <a className="project-link" href={gitHLink} target="_blank" rel="noopener noreferrer">
+            GitHub Repository
+            </a>
         </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Project
+export default Project;
