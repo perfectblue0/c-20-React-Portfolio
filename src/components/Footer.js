21b1b0ea0c3196c2e4af1b0f60585e6535d9b6d2
Paola_G <perfectblue@live.com>
2023-08-13 14:28:35 -0700

    installed react-icons and added github, linkedin, and stack overflow font awesome icons w/ links to my profiles in footer, updated README tasks list

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { FaGithub, FaLinkedin, FaStackOverflow } from 'react-icons/fa';
 
 function Footer() {
     return (
-        <footer>
-            <h4>Will add images of github(w/link to profile) and image of linkedIn</h4>
-        </footer>
-    )
+      <footer>
+        <div>
+          <a href="https://github.com/perfectblue0">
+            <FaGithub size={32} />
+          </a>
+          <a href="https://linkedin.com/in/paola-g-lupercio/">
+            <FaLinkedin size={32} />
+          </a>
+          <a href="https://stackoverflow.com/users/22384896/perfectblue-000">
+            <FaStackOverflow size={32} />
+          </a>
+        </div>
+      </footer>
+    );
 }
 
 export default Footer;
