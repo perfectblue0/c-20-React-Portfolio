56590510a97531d7061c9f2817a2b3242d5dd6c4
Paola_G <perfectblue@live.com>
2023-08-13 13:43:30 -0700

    added allSkills array with skills categories(contain associated skills) to Resume component

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React from "react";
+import allSkills from "./resumeData";
 
 export default function Resume() {
-  const resume = require('../../assets/fake-resume.pdf'); 
+  const resume = require("../../assets/fake-resume.pdf");
   return (
     <div>
       <section>
         <h2>My Developer Proficiencies</h2>
         <div>
-          <ul>
-            
-          </ul>
+          {allSkills.map((category) => (
+            <div key={category.category}>
+              <h2>{category.category}</h2>
+              <ul>
+                {category.skills.map((skill, index) => (
+                  <li key={index}>
+                    <strong>{skill.name}</strong> - {skill.description}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </section>
-      <p>Download my resume:</p>
-      <a href={resume} download="fake-resume.pdf">
-        Download Resume
-      </a>
+      <div>
+        <p>Download my resume:</p>
+        <a href={resume} download="fake-resume.pdf">
+          Download Resume
+        </a>
+      </div>
     </div>
   );
 }
---
+++ src/components/pages/resumeData.js
@@ -0,0 +1,113 @@
+// data.js
+
+const allSkills = [
+  {
+    category: "Front-end Development",
+    skills: [
+      {
+        name: "HTML",
+        description: "Markup language for structuring content on the web.",
+      },
+      {
+        name: "CSS",
+        description:
+          "Styling language for designing the visual appearance of web pages.",
+      },
+      {
+        name: "JavaScript",
+        description:
+          "Programming language used for web interactivity and behavior.",
+      },
+      {
+        name: "APIs",
+        description:
+          "Working with external services to fetch and manipulate data.",
+      },
+    ],
+  },
+  {
+    category: "Version Control",
+    skills: [
+      {
+        name: "Git",
+        description:
+          "Distributed version control system for tracking changes in code.",
+      },
+      {
+        name: "GitHub",
+        description:
+          "Platform for hosting and collaborating on Git repositories.",
+      },
+    ],
+  },
+  {
+    category: "Back-end Development",
+    skills: [
+      {
+        name: "Node.js",
+        description:
+          "JavaScript runtime environment for server-side development.",
+      },
+      {
+        name: "Jest",
+        description: "JavaScript testing framework for unit testing.",
+      },
+      {
+        name: "Express.js",
+        description:
+          "Web application framework for building APIs and server-side applications.",
+      },
+      { name: "MySQL", description: "Relational database management system." },
+      {
+        name: "Object-Relational Mapping (ORM)",
+        description:
+          "Technique for mapping database objects to programming language objects.",
+      },
+    ],
+  },
+  {
+    category: "Front-end Frameworks",
+    skills: [
+      {
+        name: "React",
+        description: "JavaScript library for building user interfaces.",
+      },
+      {
+        name: "MongoDB",
+        description: "NoSQL database used for storing and retrieving data.",
+      },
+      { name: "Mongoose", description: "ODM library for MongoDB and Node.js." },
+      {
+        name: "Regex (Regular Expressions)",
+        description:
+          "Pattern-matching language used for string manipulation and validation.",
+      },
+    ],
+  },
+  {
+    category: "Programming Paradigms",
+    skills: [
+      {
+        name: "Object-Oriented Programming (OOP)",
+        description:
+          "Programming approach based on the concept of objects and classes.",
+      },
+      {
+        name: "Functional Programming",
+        description:
+          "Programming approach that treats computation as the evaluation of functions.",
+      },
+    ],
+  },
+  {
+    category: "Other Tools and Libraries",
+    skills: [
+      {
+        name: "Inquirer",
+        description: "Command-line interface for prompting user interactions.",
+      },
+    ],
+  },
+];
+
+export default allSkills;
