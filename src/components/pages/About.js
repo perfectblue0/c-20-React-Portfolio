20565c9c2cdd7f718a3b576a27f6f7c19557cce8
Paola_G <perfectblue@live.com>
2023-08-16 17:32:09 -0700

    added background to about page text area, changed styling to h2 tags, fixed background of body, changed position of navigation links, removed padding of navigation links, added styling to bodyText class elements

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,9 @@ import profileP from "../../assets/projectImages/profile-pic.png"
 export default function About() {
 
   return (
-    <div>
+    <div className='aboutBox'>
       <h2>About Me</h2>
-      <img className="profile-pic" alt="avatar" src={profileP} />
+      <img className="profile-pic" id="profilePic" alt="avatar" src={profileP} />
       <p className="bodyText">
         I am currently enrolled in UC Berkeley's Full-Stack Coding Bootcamp. To learn more about what I have been up to, check out my projects in my Portfolio. I have my Resume with information about the technologies I have learned as well. If you have any questions, get in touch with me and send me a direct message on my Contact page or send me an email to the address shared there.
       </p>
