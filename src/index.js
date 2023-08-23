2af6f035a19f24bce8654042203875d747f56671
Paola_G <perfectblue@live.com>
2023-08-22 19:12:14 -0700

    modified code in index.js to fix deploy issue

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/index.css'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
